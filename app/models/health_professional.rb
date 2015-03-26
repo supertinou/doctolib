7e63b01401e3d90cc466c5fc2d291d4164836961
Martin Lagrange <lagrangemartin@gmail.com>
2015-03-26 11:39:44 +0100

    The health professional should have at least one medical speciality

--- app/models/health_professional.rb
+++ app/models/health_professional.rb
@@ -1,5 +1,5 @@
 class HealthProfessional < ActiveRecord::Base
-	validates :firstname, :lastname, :address, presence: true
+	validates :firstname, :lastname, :address,:medical_specialities, presence: true
 	has_and_belongs_to_many :medical_specialities, join_table: :health_professionals_specialities
 	geocoded_by :address
 	after_validation :geocode 
